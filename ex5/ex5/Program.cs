2067c61523f2006f9da1f2eab4097c85e45d7735
Andreas Barbesgaard <Abarbesgaard@gmail.com>
2023-02-23 11:10:02 +0100

    nye ting: kransekage opskrift + ændringer i solution

--- ex5/ex5/Program.cs
+++ ex5/ex5/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ex5
+﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ex5
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+            Bil renault = new Bil();
+
+            Console.WriteLine(renault.Fart(100));
+            Console.ReadLine();
         }
     }
+
+    public class Bil
+    {
+        
+        private int speed = 0;
+        private int doors = 4;
+
+
+        public int Fart(int a)
+        {
+            speed = a;
+            return speed;
+        }
+
+    }
+
+    
+
+
 }
